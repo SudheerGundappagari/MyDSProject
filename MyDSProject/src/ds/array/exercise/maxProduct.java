f9e4d072937e6ab3425e6db325a4d2f6b084ef57
Sudheer Gundappagari <54700166+SudheerGundappagari@users.noreply.github.com>
2021-09-13 09:39:22 +0530

    array exercises and aray

---
+++ MyDSProject/src/ds/array/exercise/maxProduct.java
@@ -0,0 +1,30 @@
+package ds.array.exercise;
+
+class MaxPro {
+  public static void main(String[] args) {
+	  MaxPro mn = new MaxPro();
+    int[] intArray = {10,60,30,40,50};
+    String pairs = mn.maxProduct(intArray);
+    System.out.println(pairs);
+
+  }
+
+  // Max Product
+  public String maxProduct(int[] intArray) {
+    int maxProduct = 0;
+    String pairs = "";
+    for (int i = 0; i<intArray.length; i++) {
+      for (int j = i+1; j<intArray.length; j++) {
+        if (intArray[i]*intArray[j]>maxProduct) {
+          maxProduct = intArray[i]*intArray[j];
+          pairs = Integer.toString(intArray[i]) + "," + Integer.toString(intArray[j]);
+        }
+      }
+    }
+    return pairs;
+  }
+
+
+}
+
+  
